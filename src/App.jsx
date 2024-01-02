75a915b88479cd949e46af3e382ddd9f8eed1387
CosmeNicolas <nicomas7590@gmail.com>
2024-01-02 18:26:47 -0300

    inicialice el proyecto de lista de tareas con localstorage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import {Container} from 'react-bootstrap'
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+    <Container className='main'>
+      <h1 className='text-center text-light'>Lista Tareas</h1>
+    </Container>
+     <Footer></Footer>
+    </>
+  )
+}
+
+export default App
