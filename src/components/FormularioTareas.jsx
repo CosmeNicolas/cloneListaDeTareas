a7086079a4702dac4a6faba589e53bb0af649352
CosmeNicolas <nicomas7590@gmail.com>
2024-03-19 13:25:28 -0300

    agregue el nombreTarea al formulario

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -50,7 +50,8 @@ const FormularioTareas = () => {
     <Form className="text-center" onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className="text-light">Tareas 📝</Form.Label>
-        <Form.Control 
+        <Form.Control
+        name="nombreTarea" 
         type="text"
         placeholder="ingresar Tarea: ej Tarea 1"
         minLength={3}
