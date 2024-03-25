8e7aa6adee53689615fbd3a9493fc56a07afe90f
CosmeNicolas <nicomas7590@gmail.com>
2024-03-25 17:49:43 -0300

    saque la funcion guardar tareas en el state que estaba relacionada con el guardado en el localStorage

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2'
 const FormularioTareas = () => {
 
   const [tarea, setTarea] = useState('');
-  const [tareas, setTareas] = useState(guardarTareas);
+  const [tareas, setTareas] = useState([]);
 
  useEffect(() => {
   
---
+++ src/components/helpers/queries.js
@@ -0,0 +1,1 @@
+const URI_TAREAS=import.meta.env.VITE_API_TAREAS
